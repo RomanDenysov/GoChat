71dfd2cf07167da79444ba551b4935f7e6304f3f
Roman_home_pc <romandenysovsk@gmail.com>
2023-12-18 10:06:59 +0100

    dev 1.1 <add client interface>

--- cmd/gochat/main.go
+++ cmd/gochat/main.go
@@ -6,8 +6,12 @@ import (
 
 	"github.com/gorilla/websocket"
 )
+type Client struct {
+    conn     *websocket.Conn // Соединение WebSocket.
+    username string          // Имя пользователя.
+}
 
-var clients = make(map[*websocket.Conn]bool)
+var clients = make(map[*Client]bool)
 var broadcast = make(chan []byte)
 
 var upgrader = websocket.Upgrader{
@@ -17,49 +21,64 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
-	http.HandleFunc("/ws", handleConnections)
-	go handleMessages()
+	 // Установка обработчика WebSocket
+	 http.HandleFunc("/ws", handleConnections)
+	 go handleMessages()
+ 
+	 // Настройка сервера для обслуживания статических файлов из папки сборки React
+	//  fs := http.FileServer(http.Dir("dist"))
+	//  http.Handle("/", fs)
+ 
+	 // Запуск HTTP сервера
+	 log.Println("HTTP server started on :8080")
+	 err := http.ListenAndServe(":8080", nil)
+	 if err != nil {
+		 log.Fatal("ListenAndServe: ", err)
+	 }
+ }
 
-	log.Println("http server started on: 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
-}
+ func handleConnections(w http.ResponseWriter, r *http.Request) {
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer ws.Close()
 
-func handleConnections(w http.ResponseWriter, r *http.Request) {
+    ws.WriteMessage(websocket.TextMessage, []byte("Введите ваше имя пользователя:"))
+    _, usernameMsg, err := ws.ReadMessage()
+    if err != nil {
+        log.Println("Error reading username:", err)
+        return
+    }
+    username := string(usernameMsg)
+	log.Println("Username:", username)
+	
+    client := &Client{conn: ws, username: username}
+    clients[client] = true
 
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ws.Close()
-
-	clients[ws] = true
-
-	for { 
-		var msg []byte
-		_, msg, err = ws.ReadMessage()
-		if err != nil {
-			log.Printf("error: %v", err)
-			delete(clients, ws)
-			break
-		}
-		broadcast <- msg
-	}
+    for {
+        _, msg, err := ws.ReadMessage()
+        if err != nil {
+            log.Printf("error: %v", err)
+            delete(clients, client)
+            break
+        }
+        broadcast <- msg
+    }
 }
 
 func handleMessages() {
-	for {
-		msg := <-broadcast
+    for {
+        msg := <-broadcast
 
-		for client := range clients {
-			err := client.WriteMessage(websocket.TextMessage, msg)
-			if err != nil {
-				log.Printf("error: %v", err)
-				client.Close()
-				delete(clients, client)
-			}
-		}
-	}
+        for client := range clients {
+            // Здесь мы обращаемся к `conn`, которое является частью структуры `Client`
+            err := client.conn.WriteMessage(websocket.TextMessage, msg)
+            if err != nil {
+                log.Printf("error: %v", err)
+                client.conn.Close() // Закрываем соединение
+                delete(clients, client)
+            }
+        }
+    }
 }
